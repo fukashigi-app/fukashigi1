4a80937657c4f3ec126a8e79bb324398d38c2b33
fukahsigi <0611tatsuki0611@gmail.com>
2026-05-09 18:09:04 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,19 @@
-// あなたのFUKASHIGIプロジェクト専用の設定値
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDt2LBQ7W_k2XGOYw273AozZ2-sY2i4z6k",
+  // ※ ここは先ほど直したご自身の本物のAPIキーを入れてください
+  apiKey: "AIzaSyDt2LBQ7W_k2XGOYw273AozZ2-sY2i4z6k", 
   authDomain: "fukashigi-1.firebaseapp.com",
   projectId: "fukashigi-1",
   storageBucket: "fukashigi-1.firebasestorage.app",
   messagingSenderId: "130242994746",
-  appId: "1:130242994746:web:0dd04ccf2f0c74dbeb8b03"
+  appId: "1:130242994746:web:0dd04ccf2f0c74dbeb8b03",
+  measurementId: "G-7S8QM44H4Q"
 };
 
 // Firebaseの初期化（Compat版の書き方）
 firebase.initializeApp(firebaseConfig);
 
-// 認証(Auth)とデータベース(Firestore)を使えるように変数に入れる
-const auth = firebase.auth();
-const db = firebase.firestore();
+// index.html の 79行目と81行目で auth と db をそのまま使えるようにする魔法
+window.auth = firebase.auth();
+window.db = firebase.firestore();
